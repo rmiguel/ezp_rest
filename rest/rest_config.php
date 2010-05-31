3bd49880f8366362a20cd8b2d403204fcb2116a5
Ole Marius Smestad <oms@ez.no>
2010-05-31 14:45:53 +0200

    Added barebones structure

---
+++ rest/rest_config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Rest bootstrapper
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ * @version //autogentag//
+ * @package rest
+ */
+
+require '../autoload.php';
+
+
+?>
