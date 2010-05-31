17a57264a64f382e04a3af0ddf4e1750ccecbd54
Bertrand Dunogier <bd@ez.no>
2010-05-31 18:34:16 +0200

    Renamed read.php to content.php Added a controller class skeleton + methods

---
+++ rest/classes/controllers/content.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * File containing the ezpContentRestController class.
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ */
+
+/**
+ * This controller is used for serving content
+ */
+class ezpRestContentController extends ezcMvcController
+{
+
+    public function doList( $rootNodeId = false )
+    {
+
+    }
+
+    public function doDisplay( $contentId )
+    {
+
+    }
+
+    public function doFields( $contentId )
+    {
+
+    }
+}
+?>
