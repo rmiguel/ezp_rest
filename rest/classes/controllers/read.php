edbb39dac53ebe158bad14abe02a60470cdba65a
Ole Marius Smestad <oms@ez.no>
2010-06-01 03:06:28 +0200

    Removed unused controller.

--- rest/classes/controllers/read.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * File containing controller for serving content
- *
- * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
- * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
- */
-
-?>
