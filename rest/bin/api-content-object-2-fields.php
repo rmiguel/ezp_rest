322a34c3e00fe552407c3ddad19e8fb5d337f45d
Bertrand Dunogier <bd@ez.no>
2010-06-02 19:14:33 +0200

    Renamed a bin test file

--- rest/bin/api-content-object-2-fields.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Crude bin test script
- * Simulates GET /api/content/node/2 and outputs the ezcMvcResult
- */
-$controller = new ezpRestContentController( 'viewFields', new ezcMvcRequest );
-$result = $controller->doViewFields( 1 );
-print_r( $result );
-?>
