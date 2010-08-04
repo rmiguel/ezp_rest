2e4940a2dcbfdbe964aa5ed0623b4f43fcb5cea9
Ole Marius Smestad <oms@ez.no>
2010-08-04 03:22:38 +0200

    Added: More status codes

--- rest/classes/http_response_codes.php
+++ rest/classes/http_response_codes.php
@@ -2,6 +2,9 @@
 class ezpHttpResponseCodes
 {
     const OK = 200;
+    const BAD_REQUEST = 400;
+    const UNAUTHORIZED = 401;
+    const FORBIDDEN = 403;
     const NOT_FOUND = 404;
     const SERVER_ERROR = 500;
 }
