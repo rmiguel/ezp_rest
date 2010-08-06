88b75d222bcd08d57f76f14f572e3545237c1005
Ole Marius Smestad <oms@ez.no>
2010-08-06 06:43:07 +0200

    Updated: Moved basic auth out of the dispatcher configuration

--- rest/classes/auth/styles/basic_auth.php
+++ rest/classes/auth/styles/basic_auth.php
@@ -4,10 +4,41 @@ class ezpRestBasicAuthStyle implements ezpRestAuthenticationStyle
 {
     public function setup( ezcMvcRequest $request )
     {
+        // Testing basic auth
+        $logger = ezcLog::getInstance();
+        $logger->source = __FUNCTION__;
+        $logger->category = "auth";
+
+        if ( $request->authentication === null )
+        {
+            $logger->log( "No credentials available", ezcLog::DEBUG );
+            $request->uri = '/http-basic-auth';
+            return new ezcMvcInternalRedirect( $request );
+        }
+
+        $cred = new ezcAuthenticationPasswordCredentials( $request->authentication->identifier,
+                                                          md5( "{$request->authentication->identifier}\n{$request->authentication->password}" ) );
+        $authDbInfo = new ezcAuthenticationDatabaseInfo( ezcDbInstance::get(), 'ezuser', array( 'login', 'password_hash' ) );
+
+        $auth = new ezcAuthentication( $cred );
+        $auth->addFilter( new ezcAuthenticationDatabaseFilter( $authDbInfo ) );
+        return $auth;
     }
 
-    public function authenticate()
+    public function authenticate( ezcAuthentication $auth, ezcMvcRequest $request )
     {
+        if ( !$auth->run() )
+        {
+            // @TODO: Proper error messages required of course.
+            $request->uri = '/http-basic-auth';
+            return new ezcMvcInternalRedirect( $request );
+        }
+        else
+        {
+            // We're in
+            $logger->log( "Authentication successful", ezcLog::DEBUG );
+            // $logger->log( var_export( $request->raw, true), ezcLog::DEBUG );
+        }
     }
 }
 ?>
--- rest/classes/mvc_configuration.php
+++ rest/classes/mvc_configuration.php
@@ -71,43 +71,5 @@ class ezpMvcConfiguration implements ezcMvcDispatcherConfiguration
     {
 
     }
-
-    public function runAuthFilter( $request )
-    {
-        // $basicAuth = new ezpRestBasicAuthStyle;
-        // Testing basic auth
-        $logger = ezcLog::getInstance();
-        $logger->source = __FUNCTION__;
-        $logger->category = "auth";
-
-        if ( $request->authentication === null )
-        {
-            $logger->log( "No credentials available", ezcLog::DEBUG );
-            $request->uri = '/http-basic-auth';
-            return new ezcMvcInternalRedirect( $request );
-        }
-
-        $cred = new ezcAuthenticationPasswordCredentials( $request->authentication->identifier,
-                                                          md5( "{$request->authentication->identifier}\n{$request->authentication->password}" ) );
-        $authDbInfo = new ezcAuthenticationDatabaseInfo( ezcDbInstance::get(), 'ezuser', array( 'login', 'password_hash' ) );
-
-        $auth = new ezcAuthentication( $cred );
-        $auth->addFilter( new ezcAuthenticationDatabaseFilter( $authDbInfo ) );
-
-
-        if ( !$auth->run() )
-        {
-            // @TODO: Proper error messages required of course.
-            $request->uri = '/http-basic-auth';
-            return new ezcMvcInternalRedirect( $request );
-        }
-        else
-        {
-            // We're in
-            $logger->log( "Authentication successful", ezcLog::DEBUG );
-            // $logger->log( var_export( $request->raw, true), ezcLog::DEBUG );
-        }
-    }
-
 }
 ?>
