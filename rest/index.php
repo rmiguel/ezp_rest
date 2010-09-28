7024ca39af1710e6cab1cdff5749d4d2533b1a13
Ole Marius Smestad <oms@ez.no>
2010-09-28 16:13:50 +0200

    Fix: license header in bootstrap file

--- rest/index.php
+++ rest/index.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * File containing the rest bootstrap
+ *
+ * @copyright Copyright (C) 1999-2010 eZ Systems AS. All rights reserved.
+ * @license http://ez.no/licenses/gnu_gpl GNU GPLv2
+ *
+ */
+
 require 'autoload.php';
 require 'rest/rest/classes/lazy.php';
 
